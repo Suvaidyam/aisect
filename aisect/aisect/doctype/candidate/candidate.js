6931696ee8ea295cd41bf347419f2cf94a65c3af
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-05-29 10:04:35 +0000

    aadhar vailidation apply

--- aisect/aisect/doctype/candidate/candidate.js
+++ aisect/aisect/doctype/candidate/candidate.js
@@ -1,17 +1,35 @@
 // Copyright (c) 2024, Rahul Sah and contributors
 // For license information, please see license.txt
 
+let aadharPattern = /^\d{4}\d{4}\d{4}$/;
 frappe.ui.form.on("Candidate", {
     refresh(frm) {
-        depended_dropdown(frm,frm.doc.zone,'state','zone')
-        depended_dropdown(frm,frm.doc.state,'centre','state')
-	},
+        depended_dropdown(frm, frm.doc.zone, 'state', 'zone')
+        depended_dropdown(frm, frm.doc.state, 'centre', 'state')
+    },
+    validate(frm){
+        if (!aadharPattern.test(frm.doc.aadhar_number)) {
+            frappe.throw('Enter vaild aadhar number')
+        }
+    },
+    after_save(frm){
+        if (!aadharPattern.test(frm.doc.aadhar_number)) {
+            frappe.throw('Enter vaild aadhar number')
+        }
+    },
     zone: function (frm) {
-        depended_dropdown(frm,frm.doc.zone,'state','zone')
+        depended_dropdown(frm, frm.doc.zone, 'state', 'zone')
         frm.set_value('state', '')
     },
     state: function (frm) {
-        depended_dropdown(frm,frm.doc.state,'centre','state')
+        depended_dropdown(frm, frm.doc.state, 'centre', 'state')
         frm.set_value('centre', '')
+    },
+    aadhar_number: function (frm) {
+        if (frm.doc.aadhar_number.length > 11) {
+            if (!aadharPattern.test(frm.doc.aadhar_number)) {
+                frappe.throw('Enter vaild aadhar number')
+            }
+        }
     }
 });
--- aisect/aisect/doctype/candidate/candidate.py
+++ aisect/aisect/doctype/candidate/candidate.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2024, Rahul Sah and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Candidate(Document):
-	pass
+	def after_insert(self):
+		print('*****************************************',self.batch)
+		doc = frappe.get_doc({
+			"doctype":"Candiate Placement Details",
+			"candidate_name": self.name,
+			"zone" : self.zone,
+			"state" : self.state,
+			"centre" : self.centre
+		})
+		doc.save(ignore_permissions=True)
+	def on_update(self):
+		print('*****************************************',self.batch)
