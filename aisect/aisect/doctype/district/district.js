4e263a6c5abbd17121a4c167447d080aabd9a025
bittu9835 <bk2671111@gmail.com>
2024-05-30 16:13:15 +0530

    give validation

--- aisect/aisect/doctype/district/district.js
+++ aisect/aisect/doctype/district/district.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, Rahul Sah and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("District", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("District", {
+    refresh(frm) {
+        depended_dropdown(frm, frm.doc.zone, 'state', 'zone')
+    },
+    zone: function (frm) {
+        depended_dropdown(frm, frm.doc.zone, 'state', 'zone')
+        frm.set_value('state', '')
+    },
+});
