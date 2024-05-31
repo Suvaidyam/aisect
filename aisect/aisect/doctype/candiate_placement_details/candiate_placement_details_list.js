4a0b4df61e26a7a5ec9702c9b3eb12279fe1829f
bittu9835 <bk2671111@gmail.com>
2024-05-31 11:45:15 +0530

    hide all listview icons

--- aisect/aisect/doctype/candiate_placement_details/candiate_placement_details_list.js
+++ aisect/aisect/doctype/candiate_placement_details/candiate_placement_details_list.js
@@ -1,5 +1,9 @@
-// frappe.listview_settings['Candiate Placement Details'] = {
-//     onload: function (listview) {
-//         $('.layout-side-section').hide();
-//     }
-// };
+frappe.listview_settings['Candiate Placement Details'] = {
+    onload: function (listview) {
+        $('.layout-side-section').hide();
+    },
+    refresh: function (listview) {
+        $(".list-row-activity").hide();
+        $("use.like-icon").hide();
+    }
+};
--- aisect/aisect/doctype/candidate_enrollment/candidate_enrollment_list.js
+++ aisect/aisect/doctype/candidate_enrollment/candidate_enrollment_list.js
@@ -1,5 +1,9 @@
 frappe.listview_settings['Candidate Enrollment'] = {
     onload: function (listview) {
         $('.layout-side-section').hide();
+    },
+    refresh: function (listview) {
+        $(".list-row-activity").hide();
+        $("use.like-icon").hide();
     }
 };
