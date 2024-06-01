326a42c7f158013bfdde11d56f32d6750e8bada4
bittu9835 <bk2671111@gmail.com>
2024-06-01 11:54:14 +0530

    add docType placement history

--- aisect/aisect/doctype/candiate_placement_details/candiate_placement_details.py
+++ aisect/aisect/doctype/candiate_placement_details/candiate_placement_details.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2024, Rahul Sah and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class CandiatePlacementDetails(Document):
-	pass
+    def on_update(self):
+        doc = frappe.get_doc({
+            "doctype": "Placement History",
+            "candidate_name": self.candidate_name,
+            "modified_at": self.modified,
+            "modified_by1": self.modified_by
+        })
+        doc.save(ignore_permissions=True)
