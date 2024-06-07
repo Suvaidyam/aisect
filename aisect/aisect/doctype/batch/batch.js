e1d61905ab591229dfa21096a6651f2a7e54007f
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-06-07 15:53:21 +0530

    pass date not select

--- aisect/aisect/doctype/batch/batch.js
+++ aisect/aisect/doctype/batch/batch.js
@@ -6,6 +6,11 @@ frappe.ui.form.on("Batch", {
         depended_dropdown(frm, frm.doc.state, 'district', 'state')
         depended_dropdown(frm, frm.doc.district, 'centre', 'district')
         depended_dropdown(frm, frm.doc.sector, 'job_role', 'sector')
+        const today = new Date(); 
+        frm.fields_dict.start_date.$input.datepicker({minDate: today});
+        frm.fields_dict.end_date.$input.datepicker({minDate: today});
+        frm.fields_dict.expected_assessment_date.$input.datepicker({minDate: today});
+        frm.fields_dict.actual_assessment_date.$input.datepicker({minDate: today});
     },
 
     zone: function (frm) {
@@ -30,13 +35,21 @@ frappe.ui.form.on("Batch", {
         frm.is_assessment_date_being_set = false;
         frm.is_actual_assessment_date_being_set = false;
     },
+    start_date: function (frm) {
+        const today = new Date(frm.doc.start_date); 
+        frm.fields_dict.end_date.$input.datepicker({minDate: today});
+    },
     end_date: function (frm) {
+        const today = new Date(frm.doc.end_date); 
+        frm.fields_dict.expected_assessment_date.$input.datepicker({minDate: today});
         date_validation(frm, frm.doc.end_date, frm.doc.start_date, frm.is_end_date_being_set, 'end_date', 'Start Date', 'End Date')
     },
     expected_assessment_date: function (frm) {
+        const today = new Date(frm.doc.expected_assessment_date); 
+        frm.fields_dict.actual_assessment_date.$input.datepicker({minDate: today});
         date_validation(frm, frm.doc.expected_assessment_date, frm.doc.end_date, frm.is_expected_assessment_date_being_set, 'expected_assessment_date', 'End Date', 'Expected Assessment Date')
     },
     actual_assessment_date: function (frm) {
-        date_validation(frm, frm.doc.actual_assessment_date, frm.doc.assessment_date, frm.is_actual_assessment_date_being_set, 'actual_assessment_date', 'Assessment date', 'Actual assessment date')
+       date_validation(frm, frm.doc.actual_assessment_date, frm.doc.assessment_date, frm.is_actual_assessment_date_being_set, 'actual_assessment_date', 'Assessment date', 'Actual assessment date')
     }
 });
