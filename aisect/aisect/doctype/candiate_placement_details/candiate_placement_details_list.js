93fc51ab4852b98c6c519053c7105184f91b57f5
Sujeet Yadav <sujeetyadav.suvaidyam@gmail.com>
2024-06-07 15:49:59 +0530

    hide section

--- aisect/aisect/doctype/candiate_placement_details/candiate_placement_details_list.js
+++ aisect/aisect/doctype/candiate_placement_details/candiate_placement_details_list.js
@@ -1,11 +1,14 @@
 frappe.listview_settings['Candiate Placement Details'] = {
     onload: function (listview) {
         $('.layout-side-section').hide();
+        $('.sidebar-section.filter-section').hide();
+        $('.sidebar-section.save-filter-section').hide();
     },
     refresh: function (listview) {
-        // console.log(listview)
         $(".list-row-activity").hide();
         $("use.like-icon").hide();
+        $('.sidebar-section.filter-section').hide();
+        $('.sidebar-section.save-filter-section').hide();
         function applyStyles(status, bgColor, textColor) {
             var elements = document.querySelectorAll(`.list-row-col.ellipsis.hidden-xs [data-filter="placement_status,=,${status}"]`);
             elements.forEach(function (element) {
