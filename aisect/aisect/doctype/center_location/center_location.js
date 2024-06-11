735e13b8033bcff9dc30309ac2d2830ab41e5c16
Sujeet Yadav <sujeetyadav.suvaidyam@gmail.com>
2024-06-11 16:18:34 +0530

    add placeholder

--- aisect/aisect/doctype/center_location/center_location.js
+++ aisect/aisect/doctype/center_location/center_location.js
@@ -5,10 +5,17 @@ frappe.ui.form.on("Center Location", {
     refresh(frm) {
         depended_dropdown(frm, frm.doc.zone, 'state', 'zone');
         depended_dropdown(frm, frm.doc.state, 'district', 'state')
-        if(frm.doc.center_location_code!=undefined){
-            frm.set_df_property('center_location_code','read_only',1)
+        if (frm.doc.center_location_code != undefined) {
+            frm.set_df_property('center_location_code', 'read_only', 1)
         }
+        setPlaceholders(frm, [
+            { fieldName: 'center_location_name', placeholderText: __("Enter your center location name"), placeholderSize: "12px", placeholderFontWeight: 400 },
+            { fieldName: 'center_location_code', placeholderText: __("Enter your center location code"), placeholderSize: "12px", placeholderFontWeight: 400 },
+            { fieldName: 'zone', placeholderText: __("Enter your zone"), placeholderSize: "12px", placeholderFontWeight: 400 },
+            { fieldName: 'state', placeholderText: __("Enter your state"), placeholderSize: "12px", placeholderFontWeight: 400 },
+            { fieldName: 'district', placeholderText: __("Enter your district"), placeholderSize: "12px", placeholderFontWeight: 400 },
 
+        ])
     },
 
     zone: function (frm) {
@@ -19,10 +26,10 @@ frappe.ui.form.on("Center Location", {
         depended_dropdown(frm, frm.doc.state, 'district', 'state')
         frm.set_value('district', '')
     },
-    after_save:function(frm){
-        if(frm.doc.center_location_code!=undefined){
-            frm.set_df_property('center_location_code','read_only',1)
+    after_save: function (frm) {
+        if (frm.doc.center_location_code != undefined) {
+            frm.set_df_property('center_location_code', 'read_only', 1)
         }
     },
-    
+
 });
