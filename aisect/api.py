351195fcdff444fb4f1f335107a24349a9b6e7a1
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-06-18 18:00:15 +0530

    all master show active data

--- aisect/api.py
+++ aisect/api.py
@@ -9,7 +9,7 @@ def get_user_role():
 @frappe.whitelist()
 def set_candidate_status():
     current_date = date.today()
-    items = frappe.db.get_list('Batch', fields=['name','start_date','end_date', 'expected_assessment_date'])
+    items = frappe.db.get_list('Batch', fields=['name','start_date','end_date', 'expected_assessment_date','actual_assessment_date'])
     
     for item in items:
         # batch status
@@ -21,19 +21,29 @@ def set_candidate_status():
             frappe.db.set_value('Batch', item.name,'status','In Progress')
 
         # candidate status
-        if item.expected_assessment_date == current_date:
+        if item.expected_assessment_date <= current_date:
             candidates = frappe.db.get_list('Candidate Details', fields=['name'], filters={'batch_id': item.name,'assessment_status':'Registered'})
             
             for candidate in candidates:
-                date_90_days = current_date + timedelta(days=90)
                 try:
-                    frappe.db.set_value('Candidate Details', candidate.name,
-                        {
-                            'current_status':'Assessed',
-                            'assessment_status':'Assessed',
-                            'assessment_date':current_date,
-                            'placement_due_date':date_90_days
-                        })
+                    if item.expected_assessment_date and item.actual_assessment_date:
+                        date_90_days = current_date + timedelta(days=90)
+                        frappe.db.set_value('Candidate Details', candidate.name,
+                            {
+                                'current_status':'Assessed',
+                                'assessment_status':'Assessed',
+                                'assessment_date':current_date,
+                                'placement_due_date':date_90_days
+                            })
+                    else:
+                         date_90_days = current_date + timedelta(days=90)
+                         frappe.db.set_value('Candidate Details', candidate.name,
+                            {
+                                'current_status':'Assessed',
+                                'assessment_status':'Assessed',
+                                'assessment_date':current_date,
+                                'placement_due_date':date_90_days
+                            })
                 except Exception as e:
                     print(f"Error updating candidate {candidate['name']}: {e}")
     frappe.db.commit()
