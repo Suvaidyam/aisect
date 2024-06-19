254c739c3e1d3b40fc1005035b73033ac79e781a
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-06-19 11:23:12 +0530

    err fixed

--- aisect/aisect/doctype/job_role/job_role.js
+++ aisect/aisect/doctype/job_role/job_role.js
@@ -3,6 +3,7 @@
 
 frappe.ui.form.on("Job Role", {
     refresh(frm) {
+        check_active(frm,'sector')
         setPlaceholders(frm, [
             { fieldName: 'job_role_name', placeholderText: __("Enter your job role name") },
             { fieldName: 'sector', placeholderText: __("Enter your sector") },
