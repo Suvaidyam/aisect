0f0306ee5efda0bcf5ac412f8988aaffd8a009b6
bittu9835 <bk2671111@gmail.com>
2024-06-21 16:48:32 +0530

    change lable center location to center

--- aisect/aisect/doctype/candidate_details/candidate_details.js
+++ aisect/aisect/doctype/candidate_details/candidate_details.js
@@ -5,59 +5,59 @@ let mobilePattern = /^[6-9]\d{9}$/;
 
 frappe.ui.form.on("Candidate Details", {
     refresh(frm) {
-        check_active(frm,'project')
+        check_active(frm, 'project')
         frm.fields_dict['zone'].get_query = function () {
             return {
-                filters: {'zone_name':'Please select batch id'},
+                filters: { 'zone_name': 'Please select batch id' },
                 page_length: 1000
             };
         }
         frm.fields_dict['state'].get_query = function () {
             return {
-                filters: {'state_name':'Please select batch id'},
+                filters: { 'state_name': 'Please select batch id' },
                 page_length: 1000
             };
         }
         frm.fields_dict['district'].get_query = function () {
             return {
-                filters: {'district_name':'Please select batch id'},
+                filters: { 'district_name': 'Please select batch id' },
                 page_length: 1000
             };
         }
         frm.fields_dict['center_location'].get_query = function () {
             return {
-                filters: {'center_location_name':'Please select batch id'},
+                filters: { 'center_location_name': 'Please select batch id' },
                 page_length: 1000
             };
         }
         frm.fields_dict['sector'].get_query = function () {
             return {
-                filters: {'sector_name':'Please select batch id'},
+                filters: { 'sector_name': 'Please select batch id' },
                 page_length: 1000
             };
         }
         frm.fields_dict['job_role'].get_query = function () {
             return {
-                filters: {'job_role_name':'Please select batch id'},
+                filters: { 'job_role_name': 'Please select batch id' },
                 page_length: 1000
             };
         }
-        hide_advance_search(frm,['project','batch_id','zone','state','district','center_location','sector','job_role'])
-        if(frm.doc.batch_id){
+        hide_advance_search(frm, ['project', 'batch_id', 'zone', 'state', 'district', 'center_location', 'sector', 'job_role'])
+        if (frm.doc.batch_id) {
             setTimeout(() => {
-                frm.set_df_property('zone','read_only',1)
-                frm.set_df_property('state','read_only',1)
-                frm.set_df_property('district','read_only',1)
-                frm.set_df_property('center_location','read_only',1)
-                frm.set_df_property('sector','read_only',1)
-                frm.set_df_property('job_role','read_only',1)
+                frm.set_df_property('zone', 'read_only', 1)
+                frm.set_df_property('state', 'read_only', 1)
+                frm.set_df_property('district', 'read_only', 1)
+                frm.set_df_property('center_location', 'read_only', 1)
+                frm.set_df_property('sector', 'read_only', 1)
+                frm.set_df_property('job_role', 'read_only', 1)
             }, 500);
         }
-        if(frm.doc.assessment_status!='Assessed'){
-            frm.set_df_property('certified_status','read_only',1)
+        if (frm.doc.assessment_status != 'Assessed') {
+            frm.set_df_property('certified_status', 'read_only', 1)
         }
-        if(frm.doc.certified_status!='Certified'){
-            frm.set_df_property('placement_status','read_only',1)
+        if (frm.doc.certified_status != 'Certified') {
+            frm.set_df_property('placement_status', 'read_only', 1)
         }
         depended_dropdown(frm, frm.doc.project, 'batch_id', 'project')
 
@@ -75,7 +75,7 @@ frappe.ui.form.on("Candidate Details", {
             { fieldName: 'zone', placeholderText: __("Enter your zone") },
             { fieldName: 'state', placeholderText: __("Enter your state") },
             { fieldName: 'district', placeholderText: __("Enter your district") },
-            { fieldName: 'center_location', placeholderText: __("Enter your center location") },
+            { fieldName: 'center_location', placeholderText: __("Enter your center") },
             { fieldName: 'sector', placeholderText: __("Enter your sector") },
             { fieldName: 'job_role', placeholderText: __("Enter your job role") },
 
@@ -83,20 +83,20 @@ frappe.ui.form.on("Candidate Details", {
 
     },
     batch_id: function (frm) {
-        if(frm.doc.batch_id){
+        if (frm.doc.batch_id) {
             setTimeout(() => {
-                frm.set_df_property('zone','read_only',1)
-                frm.set_df_property('state','read_only',1)
-                frm.set_df_property('district','read_only',1)
-                frm.set_df_property('center_location','read_only',1)
-                frm.set_df_property('sector','read_only',1)
-                frm.set_df_property('job_role','read_only',1)
+                frm.set_df_property('zone', 'read_only', 1)
+                frm.set_df_property('state', 'read_only', 1)
+                frm.set_df_property('district', 'read_only', 1)
+                frm.set_df_property('center_location', 'read_only', 1)
+                frm.set_df_property('sector', 'read_only', 1)
+                frm.set_df_property('job_role', 'read_only', 1)
             }, 500);
         }
     },
     validate(frm) {
         if (frm.doc.aadhar_number) {
-            if(!isValidAadhaar(frm.doc.aadhar_number)){
+            if (!isValidAadhaar(frm.doc.aadhar_number)) {
                 frappe.throw('Enter vaild aadhar number')
             }
         }
@@ -106,7 +106,7 @@ frappe.ui.form.on("Candidate Details", {
     },
     after_save(frm) {
         if (frm.doc.aadhar_number) {
-            if(!isValidAadhaar(frm.doc.aadhar_number)){
+            if (!isValidAadhaar(frm.doc.aadhar_number)) {
                 frappe.throw('Enter vaild aadhar number')
             }
         }
@@ -118,7 +118,7 @@ frappe.ui.form.on("Candidate Details", {
         frm.fields_dict['batch_id'].get_query = function () {
             return {
                 filters: {
-                    project:frm.doc.project,
+                    project: frm.doc.project,
                     end_date: ['>=', frappe.datetime.now_date()]
                 },
                 page_length: 1000
@@ -127,24 +127,24 @@ frappe.ui.form.on("Candidate Details", {
         frm.set_value('batch_id', '')
     },
     assessment_status: function (frm) {
-        if(frm.doc.assessment_status=='Assessed'){
-            frm.set_df_property('certified_status','read_only',0)
+        if (frm.doc.assessment_status == 'Assessed') {
+            frm.set_df_property('certified_status', 'read_only', 0)
         }
     },
     certified_status: function (frm) {
-        if(frm.doc.certified_status!=='Certified'){
+        if (frm.doc.certified_status !== 'Certified') {
             frm.set_value('placement_status', 'N/A')
             frm.set_value('certification_date', '')
             frm.set_value('placement_date', '')
         }
-        if(frm.doc.certified_status=='Certified'){
-            frm.set_df_property('placement_status','read_only',0)
-        }else{
-            frm.set_df_property('placement_status','read_only',1)
+        if (frm.doc.certified_status == 'Certified') {
+            frm.set_df_property('placement_status', 'read_only', 0)
+        } else {
+            frm.set_df_property('placement_status', 'read_only', 1)
         }
     },
     placement_status: function (frm) {
-        if(frm.doc.placement_status!=='Placed'){
+        if (frm.doc.placement_status !== 'Placed') {
             frm.set_value('placement_date', '')
         }
     },
@@ -167,9 +167,9 @@ frappe.ui.form.on("Candidate Details", {
 
 
 // child Table
-const truncate_child_table_field_value = async(row,frm,fields) => {
-    if(fields.length >0){
-        for (let field of fields){
+const truncate_child_table_field_value = async (row, frm, fields) => {
+    if (fields.length > 0) {
+        for (let field of fields) {
             row[field] = ''
         }
         frm.cur_grid.refresh()
@@ -205,14 +205,14 @@ frappe.ui.form.on("Placement Child", {
         }
 
     },
-    employment_start_date:function(frm,cdt,cdn){
-        let row = frappe.get_doc(cdt,cdn)
+    employment_start_date: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn)
         let sd = new Date(row.employment_start_date);
         frm.cur_grid.grid_form.fields_dict.employment_end_date.$input.datepicker({ minDate: sd })
     },
-    job_joined: function (frm,cdt,cdn) {
-        let row = frappe.get_doc(cdt,cdn)
-        if(row.job_joined !== "Yes"){
+    job_joined: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.job_joined !== "Yes") {
             truncate_child_table_field_value(row, frm, [
                 'offer_letter',
                 'upload_offer_letter',
@@ -230,15 +230,15 @@ frappe.ui.form.on("Placement Child", {
                 'upload_bank_statement_3'
             ]);
         }
-        if(row.job_joined == "No"){
+        if (row.job_joined == "No") {
             truncate_child_table_field_value(row, frm, [
                 'if_no_give_reason'
             ]);
         }
     },
-    offer_letter: function (frm,cdt,cdn) {
-        let row = frappe.get_doc(cdt,cdn)
-        if(row.offer_letter !== "Yes"){
+    offer_letter: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.offer_letter !== "Yes") {
             truncate_child_table_field_value(row, frm, [
                 'upload_offer_letter',
                 'salary_slip',
@@ -256,9 +256,9 @@ frappe.ui.form.on("Placement Child", {
             ]);
         }
     },
-    salary_slip: function (frm,cdt,cdn) {
-        let row = frappe.get_doc(cdt,cdn)
-        if(row.salary_slip !== "Yes"){
+    salary_slip: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.salary_slip !== "Yes") {
             truncate_child_table_field_value(row, frm, [
                 'upload_salary_slip',
                 'bank_statement',
@@ -274,17 +274,17 @@ frappe.ui.form.on("Placement Child", {
             ]);
         }
     },
-    bank_statement: function (frm,cdt,cdn) {
-        let row = frappe.get_doc(cdt,cdn)
-        if(row.bank_statement !== "Yes"){
+    bank_statement: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.bank_statement !== "Yes") {
             truncate_child_table_field_value(row, frm, [
                 'upload_bank_statement'
             ]);
         }
     },
-    salary_slip_2: function (frm,cdt,cdn) {
-        let row = frappe.get_doc(cdt,cdn)
-        if(row.salary_slip_2 !== "Yes"){
+    salary_slip_2: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.salary_slip_2 !== "Yes") {
             truncate_child_table_field_value(row, frm, [
                 'upload_salary_slip_2',
                 'bank_statement_2',
@@ -296,17 +296,17 @@ frappe.ui.form.on("Placement Child", {
             ]);
         }
     },
-    bank_statement_2: function (frm,cdt,cdn) {
-        let row = frappe.get_doc(cdt,cdn)
-        if(row.bank_statement_2 !== "Yes"){
+    bank_statement_2: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.bank_statement_2 !== "Yes") {
             truncate_child_table_field_value(row, frm, [
                 'upload_bank_statement_2'
             ]);
         }
     },
-    salary_slip_3: function (frm,cdt,cdn) {
-        let row = frappe.get_doc(cdt,cdn)
-        if(row.salary_slip_3 !== "Yes"){
+    salary_slip_3: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.salary_slip_3 !== "Yes") {
             truncate_child_table_field_value(row, frm, [
                 'upload_salary_slip_3',
                 'bank_statement_3',
@@ -314,9 +314,9 @@ frappe.ui.form.on("Placement Child", {
             ]);
         }
     },
-    bank_statement_3: function (frm,cdt,cdn) {
-        let row = frappe.get_doc(cdt,cdn)
-        if(row.bank_statement_3 !== "Yes"){
+    bank_statement_3: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.bank_statement_3 !== "Yes") {
             truncate_child_table_field_value(row, frm, [
                 'upload_bank_statement_3'
             ]);
