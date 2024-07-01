95e00891f4e76ef7e93bf4dee84b0d1428e672cb
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-07-01 12:52:49 +0530

    bulk upload working in batch

--- aisect/aisect/doctype/batch/batch.py
+++ aisect/aisect/doctype/batch/batch.py
@@ -5,17 +5,16 @@
 from datetime import date, datetime
 from frappe.model.document import Document
 
-
 class Batch(Document):
-	def before_save(self):
-		current_date = date.today()
-		
-		self.start_date = datetime.strptime(self.start_date, '%Y-%m-%d').date()
-		self.end_date = datetime.strptime(self.end_date, '%Y-%m-%d').date()
-		
-		if self.end_date < current_date:
-			self.status = 'Completed'
-		elif self.start_date > current_date:
-			self.status = 'To Be Started'
-		elif self.start_date <= current_date <= self.end_date:
-			self.status = 'In Progress'
+    def before_save(self):
+        current_date = date.today()
+
+        self.start_date = datetime.strptime(str(self.start_date).split()[0], '%Y-%m-%d').date()
+        self.end_date = datetime.strptime(str(self.end_date).split()[0], '%Y-%m-%d').date()
+
+        if self.end_date < current_date:
+            self.status = 'Completed'
+        elif self.start_date > current_date:
+            self.status = 'To Be Started'
+        elif self.start_date <= current_date <= self.end_date:
+            self.status = 'In Progress'
