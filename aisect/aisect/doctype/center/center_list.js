782834b640461aa5abdfe8cf95494f2937a23529
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-07-05 08:05:59 +0530

    hide extra action items

--- aisect/aisect/doctype/center/center_list.js
+++ aisect/aisect/doctype/center/center_list.js
@@ -1,5 +1,8 @@
 frappe.listview_settings['Center'] = {
     onload: function (listview) {
+        if(!frappe.user_roles.includes('Administrator')){
+            action_items(listview, ['Export', 'Delete'])
+        }
         $('.layout-side-section').hide();
         $('.sidebar-section.filter-section').hide();
         $('.sidebar-section.save-filter-section').hide();
@@ -11,3 +14,16 @@ frappe.listview_settings['Center'] = {
         $("use.like-icon").hide();
     }
 };
+function action_items(listview, action) {
+    listview.page.clear_actions_menu()
+    action.map((e) => {
+        const actionItem = listview.actions_menu_items.find((item) => item?.label == e);
+        if (actionItem) {
+            listview.page.add_actions_menu_item(
+                actionItem.label,
+                actionItem.action,
+                actionItem.standard
+            );
+        }
+    })
+}
