40b93d5194a018058b8ed72e76fe3de73cd2a39d
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-07-05 11:16:03 +0530

    hide action add common

--- aisect/aisect/doctype/candidate_details/candidate_details_list.js
+++ aisect/aisect/doctype/candidate_details/candidate_details_list.js
@@ -28,16 +28,3 @@ frappe.listview_settings['Candidate Details'] = {
         applyStyles("Assessed", "#f0e6d5", "#db8904");
     }
 };
-function action_items(listview, action) { 
-    listview.page.clear_actions_menu()
-    action.map((e) => {
-        const actionItem = listview.actions_menu_items.find((item) => item?.label == e);
-        if (actionItem) {
-            listview.page.add_actions_menu_item(
-                actionItem.label,
-                actionItem.action,
-                actionItem.standard
-            );
-        }
-    })
-}
