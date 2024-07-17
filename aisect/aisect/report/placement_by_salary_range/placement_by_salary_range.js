aaad63dea0dc5091266b5cc779e0cde36c0ff0b1
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-07-17 17:17:16 +0530

    add depended filter in report

--- aisect/aisect/report/placement_by_salary_range/placement_by_salary_range.js
+++ aisect/aisect/report/placement_by_salary_range/placement_by_salary_range.js
@@ -4,43 +4,88 @@
 // For license information, please see license.txt
 var filters = [
 	{
-	  "fieldname": "batch_id",
-	  "fieldtype": "Link",
-	  "label": "Batch",
-	  "options": "Batch"
+		"fieldname": "zone",
+		"fieldtype": "Link",
+		"label": "Zone",
+		"options": "Zone",
+		"only_select":1,
 	},
-  ]
-  var state = {
-	  "fieldname": "state",
-	  "fieldtype": "Link",
-	  "label": "State",
-	  "options": "State"
-  }
-  
-  if (frappe.user_roles.includes('Head Office (PMU)')) {
-	filters.push({
-	  "fieldname": "zone",
-	  "fieldtype": "Link",
-	  "label": "Zone",
-	  "options": "Zone"
-  })
-  }
-  if (frappe.user_roles.includes('Zonal Head') || frappe.user_roles.includes('Head Office (PMU)')) {
-	filters.push({
-	  "fieldname": "state",
-	  "fieldtype": "Link",
-	  "label": "State",
-	  "options": "State"
-  })
-  }
-  if (frappe.user_roles.includes('Head Office (PMU)') || frappe.user_roles.includes('State Placement Coordinator') || frappe.user_roles.includes('State Head') || frappe.user_roles.includes('Zonal Head')) {
-	filters.push({
-	  "fieldname": "center",
-	  "fieldtype": "Link",
-	  "label": "Center",
-	  "options": "Center"
-  })
-  }
+	{
+		"fieldname": "state",
+		"fieldtype": "Link",
+		"label": "State",
+		"options": "State",
+		"only_select":1,
+		"get_query": function () {
+			var zone = frappe.query_report.get_filter_value('zone');
+			return {
+				filters: {
+					'zone': zone
+				}
+			};
+		}
+	},
+	{
+		"fieldname": "project",
+		"fieldtype": "Link",
+		"label": "Project",
+		"options": "Project",
+		"only_select":1,
+		"get_query": function () {
+			var state = frappe.query_report.get_filter_value('state');
+			return {
+				filters: [
+					['Project', 'state', 'IN', [state, '']]
+				]
+			};
+		}
+	},
+	{
+		"fieldname": "district",
+		"fieldtype": "Link",
+		"label": "District",
+		"options": "District",
+		"only_select":1,
+		"get_query": function () {
+			var state = frappe.query_report.get_filter_value('state');
+			return {
+				filters: {
+					'state': state
+				}
+			};
+		}
+	},
+	{
+		"fieldname": "center",
+		"fieldtype": "Link",
+		"label": "Center",
+		"options": "Center",
+		"only_select":1,
+		"get_query": function () {
+			var district = frappe.query_report.get_filter_value('district');
+			return {
+				filters: {
+					'district': district
+				}
+			};
+		}
+	},
+	{
+		"fieldname": "batch_id",
+		"fieldtype": "Link",
+		"label": "Batch",
+		"options": "Batch",
+		"only_select":1,
+		"get_query": function () {
+			var center = frappe.query_report.get_filter_value('center');
+			return {
+				filters: {
+					'center_location': center
+				}
+			};
+		}
+	},
+]
 frappe.query_reports["Placement By Salary Range"] = {
 	filters: filters
 };
--- aisect/aisect/report/placement_target_vs_achievement/placement_target_vs_achievement.js
+++ aisect/aisect/report/placement_target_vs_achievement/placement_target_vs_achievement.js
@@ -1,71 +1,100 @@
 // Copyright (c) 2024, Rahul Sah and contributors
 // For license information, please see license.txt
-// Copyright (c) 2024, Rahul Sah and contributors
-// For license information, please see license.txt
-
-var filters = []
-
-// if (frappe.user_roles.includes('Head Office (PMU)')) {
-//     filters.push({
-//         "fieldname": "zone",
-//         "fieldtype": "Link",
-//         "label": "Zone",
-//         "options": "Zone"
-//     })
-// }
-if (frappe.user_roles.includes('Zonal Head') || frappe.user_roles.includes('Head Office (PMU)')) {
-    filters.push({
+var filters = [
+    {
         "fieldname": "state",
         "fieldtype": "Link",
         "label": "State",
-        "options": "State"
-    })
-}
-filters.push({
-    "fieldname": "project",
-    "fieldtype": "Link",
-    "label": "Project",
-    "options": "Project"
-})
-if (frappe.user_roles.includes('Head Office (PMU)') || frappe.user_roles.includes('State Placement Coordinator') || frappe.user_roles.includes('State Head') || frappe.user_roles.includes('Zonal Head')) {
-    filters.push(
-        {
-            "fieldname": "district",
-            "fieldtype": "Link",
-            "label": "District",
-            "options": "District"
-        },
-        {
-            "fieldname": "center",
-            "fieldtype": "Link",
-            "label": "Center",
-            "options": "Center"
-        })
-}
-filters.push({
-    "fieldname": "batch_id",
-    "fieldtype": "Link",
-    "label": "Batch",
-    "options": "Batch"
-},
+        "options": "State",
+        "only_select":1,
+    },
+    {
+        "fieldname": "project",
+        "fieldtype": "Link",
+        "label": "Project",
+        "options": "Project",
+        "only_select":1,
+        "get_query": function () {
+            var state = frappe.query_report.get_filter_value('state');
+            return {
+                filters: [
+                    ['Project', 'state', 'IN', [state, '']]
+                ]
+            };
+        }
+    },
+    {
+        "fieldname": "district",
+        "fieldtype": "Link",
+        "label": "District",
+        "options": "District",
+        "only_select":1,
+        "get_query": function () {
+            var state = frappe.query_report.get_filter_value('state');
+            return {
+                filters: {
+                    'state': state
+                }
+            };
+        }
+    },
+    {
+        "fieldname": "center",
+        "fieldtype": "Link",
+        "label": "Center",
+        "options": "Center",
+        "only_select":1,
+        "get_query": function () {
+            var district = frappe.query_report.get_filter_value('district');
+            return {
+                filters: {
+                    'district': district
+                }
+            };
+        }
+    },
+
+    {
+        "fieldname": "batch_id",
+        "fieldtype": "Link",
+        "label": "Batch",
+        "options": "Batch",
+        "only_select":1,
+        "get_query": function () {
+            var center = frappe.query_report.get_filter_value('center');
+            return {
+                filters: {
+                    'center_location': center
+                }
+            };
+        }
+    },
+    {
+        "fieldname": "width",
+        "fieldtype": "Data",
+        "read_only": 1
+    },
     {
         "fieldname": "job_role",
         "fieldtype": "Link",
         "label": "Job Role",
-        "options": "Job Role"
+        "options": "Job Role",
+        "only_select":1,
     },
     {
         "fieldname": "remaining_day",
         "fieldtype": "Select",
         "label": "Remaining days",
         "options": "\n0 day\n1-30 days\n30-60 days\n60-90 days"
-    })
+    }
+]
+
 frappe.query_reports["Placement Target vs Achievement"] = {
     filters: filters,
-    formatter:function(value,k,column){
-        if(column.fieldname=="batch_id"){
+    formatter: function (value, k, column) {
+        if (column.fieldname == "batch_id") {
             return `<a href="/app/candidate-details/view/list?batch_id=${value}">${value}</a>`
-        }else{
+        } else {
             return value
         }
     }
